7baf482ec3f300f4a79c3286167fda94d381268a
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-06-15 19:48:56 -0300

    Adição de arquivos

---
+++ Aula2.js
@@ -0,0 +1,4 @@
+const primeiroNumero = 5 // Declarou a variável
+const segundoNumero = 10 // Declrarou a variável
+const resultado = primeiroNumero * segundoNumero; // Multiplicando os dois valores
+console.log(resultado) // O valor é apresentado no terminal
