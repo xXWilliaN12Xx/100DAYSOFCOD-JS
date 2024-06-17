72ce23d22de1a95d132845aaf25e00a8471144c7
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-06-17 19:19:36 -0300

    Adição de arquivos

---
+++ Aula4/Aula4.js
@@ -0,0 +1,24 @@
+/*
+    +	Adição
+    -	Subtração
+    *	Multiplicação
+    /	Divisão
+    %	Modulo
+    **	Potenciação
+*/
+
+/*
+Ordem de precedência
+
+( )		
+**		
+*	/	%
++	-	
+
+*/
+
+let numero1 = 32
+let numero2 = 12
+
+const resultado = numero1 * numero2
+console.log(`O resultado da soma foi ${resultado}`)
