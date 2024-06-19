92df6a5b01210e3da5b507ecc12c0be2f7fcaa32
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-06-19 18:56:18 -0300

    Adição dia 5

---
+++ Aula5/Aula5.js
@@ -0,0 +1,16 @@
+// if (condição) {
+// 	True
+// } else {
+// 	false
+// }
+
+let now = new Date(); // variavel now recebe nova data
+let hora = now.getHours(); // variavel hora recebe data atural
+console.log(`Agora são exatamente ${hora} horas`); // imprime o resultado no terminal em horas.
+if (hora < 12) { // condição 'se'
+  console.log("Good morning"); // imprime o resultado no terminal.
+} else if (hora <= 18) { 
+  console.log("Good afternon"); // imprime o resultado no terminal.
+} else { // condição 'senão'
+  console.log("Good night"); // imprime o resultado no terminal.
+}
