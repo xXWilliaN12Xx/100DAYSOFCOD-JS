34249447bd54efc3e90848a30a6cce8a91e938a2
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-06-18 21:45:12 -0300

    Create Aula4.js

---
+++ Aula4/Aula4.js
@@ -0,0 +1,28 @@
+// Operadores Relacionais:
+
+// >	Operador de maior
+// <	Operador de menor
+// >=	Operador de maior ou igual
+// <=	Operador de menor ou igual
+// ==	Operador de igual
+// !=	operador de não igual
+// ===	Operador de igualdade restrita
+
+// Operadores lógicos
+
+// !	Negação
+// &&	Conjunção
+// ||	Disjunção
+
+let idade = 20
+let estado = 'RJ'
+let salario = 2000
+sexo = 'F'
+
+let resultado = idade >= 15 && idade <= 17 // a idade está entre 15 e 17?
+let resultado2 = estado == 'RJ' || estado == 'SP' // o estado é RJ ou SP?
+let resultado3 = salario > 1500 && sexo != 'M' // o salário é acima de 1500 e não é homem?
+
+console.log(resultado)  // imprime o resultado no terminal: false
+console.log(resultado2) // imprime o resultado no terminal: true
+console.log(resultado3) // imprime o resultado no terminal: true
