d30a7121393bab77fc21d5a70551f74ca1f4efdf
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-06-20 22:13:07 -0300

    Adição dia 6

---
+++ Aula5/Exercicio2.js
@@ -0,0 +1,21 @@
+// Solicite a nota do usuário
+let nota = Number(70);
+
+// Verifique a classificação da nota
+
+if (nota >= 90) {
+  // calculando a nota.
+  console.log("A"); // imprime o resultado no terminal.
+} else if (nota >= 80) {
+  // calcula a nota
+  console.log("B"); // imprime o resultado no terminal.
+} else if (nota >= 70) {
+  // calcula a nota.
+  console.log("C"); // imprime o resultado no terminal.
+} else if (nota >= 60) {
+  // calcula a nota.
+  console.log("D"); // imprime o resultado no terminal.
+} else {
+  // finalização
+  console.log("F"); // imprime o resultado no terminal.
+}
