86b62084de90bef0f4e2921f1c6444ff95b1dd06
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-06-25 16:20:30 -0300

    Update script3.js

--- Day9/script3.js
+++ Day9/script3.js
@@ -1,6 +1,7 @@
-function cadete (texto) {
+function caixaAlta (texto) {
     return texto.toUpperCase();
 }
 
-console.log(cadete('better'))
-console.log(cadete('3'))
+console.log(caixaAlta('better'))
+console.log(caixaAlta('3'))
+console.log(caixaAlta('  '))
