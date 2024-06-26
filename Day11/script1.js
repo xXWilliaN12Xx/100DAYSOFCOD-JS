b73616636296ba54520198bbe4cd040d2a9417ce
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-06-26 20:54:59 -0300

    Adição dia 11

---
+++ Day11/script1.js
@@ -0,0 +1,5 @@
+let c = 1; // variavel criada com o valor 1;
+while (c <= 10) { // estrutura de repetição com teste lógico no inicio;
+    console.log(`passo ${c}`) // valor é exebido no output;
+    c++; // A variavel 'c' passa a receber 1 em 1;
+};
---
+++ Day11/script2.js
@@ -0,0 +1,5 @@
+let c = 1 // variavel criada com o valor 1;
+do { // estrutura de repetição com teste lógico no final;
+    console.log(`passo ${c}`) // valor é exebido no output;
+    c++ // A variavel 'c' passa a receber 1 em 1;
+} while (c <= 11) 
