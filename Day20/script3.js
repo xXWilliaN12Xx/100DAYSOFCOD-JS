340662884594e3e70394c7375f2fe0e741ce0e5d
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-07-06 22:15:41 -0300

    Adição Dia 20

---
+++ Day20/script3.js
@@ -0,0 +1,35 @@
+function getDiaSemanaTexto(diaSemana) {
+  let diaSemanaTexto;
+  switch (diaSemana) {
+    case 0:
+      diaSemanaTexto = "Domingo";
+      return diaSemanaTexto;
+    case 1:
+      diaSemanaTexto = "Segunda";
+      return diaSemanaTexto;
+    case 2:
+      diaSemanaTexto = "Terça";
+      return diaSemanaTexto;
+    case 3:
+      diaSemanaTexto = "Quarta";
+      return diaSemanaTexto;
+    case 4:
+      diaSemanaTexto = "Quinta";
+      return diaSemanaTexto;
+    case 5:
+      diaSemanaTexto = "Sexta";
+      return diaSemanaTexto;
+    case 6:
+      diaSemanaTexto = "Sábado";
+      return diaSemanaTexto;
+    default:
+      diaSemanaTexto = " ";
+      return diaSemanaTexto;
+  }
+}
+
+const data = new Date();
+const diaSemana = data.getDay();
+const diaSemanaTexto = getDiaSemanaTexto(diaSemana);
+
+console.log(`A data de hoje é ${diaSemana} e o dia é ${diaSemanaTexto}`);
