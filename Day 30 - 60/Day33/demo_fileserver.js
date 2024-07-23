f2e01ce375881f5a5f393ebcc40f3cfa7c78cc19
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-07-23 17:38:31 -0300

    Adição primeiro arquivo Day33

---
+++ Day 30 - 60/Day33/demo_fileserver.js
@@ -0,0 +1,19 @@
+let http = require("http");
+let url = require("url");
+let fs = require("fs");
+
+http
+  .createServer(function (req, res) {
+    let q = url.parse(req.url, true);
+    let filename = "." + q.pathname;
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
