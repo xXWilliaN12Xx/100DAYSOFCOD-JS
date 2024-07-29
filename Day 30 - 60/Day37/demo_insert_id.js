3dca3794d55077ce05fd430e8fe2423d129b92bb
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-07-29 15:59:52 -0300

    Update demo_insert_id.js

--- Day 30 - 60/Day37/demo_insert_id.js
+++ Day 30 - 60/Day37/demo_insert_id.js
@@ -12,6 +12,6 @@ con.connect(function (err) {
     let sql = "INSERT INTO customers (name, address) VALUES ('Michelle', 'Blue Village 1')";
     con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log("1 record inserted, ID: " + result.insertID);
+        console.log("1 record inserted, ID: " + result.insertId);
     });
 });
