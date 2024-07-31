baac039e413ac1ff4cb88ddee5f0c8069d5a5646
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-07-31 15:21:16 -0300

    Day39 - 3

---
+++ Day 30 - 60/Day39/demo_db_orderby.js
@@ -0,0 +1,16 @@
+let mysql = require('mysql2');
+
+let con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'xXWilliaN12Xx',
+    database: 'mydb'
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    con.query("SELECT * FROM customers ORDER BY name", function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
