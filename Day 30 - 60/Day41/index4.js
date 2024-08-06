1bd58c9dd8b7e5d822fcac9daea74577ca8e43eb
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-08-06 15:44:53 -0300

    Day41 - 4

---
+++ Day 30 - 60/Day41/index4.js
@@ -0,0 +1,8 @@
+// Escreva uma função chamada ePaisagem que receba dois parametros, largura e altura de uma imagem (numero).
+// Retorne true se a imagem estiver no modo paisagem.
+
+let ePaisagem = (largura, altura) => {
+    largura > altura
+};
+
+console.log(ePaisagem(1920, 1080));
