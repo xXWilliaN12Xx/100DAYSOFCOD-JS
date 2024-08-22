a8d36c1f61825712a11fafd4927ceec218a4bdc8
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-08-22 15:54:31 -0300

    deleção de arquivos do dia 50

--- Day 30 - 60/Day50/demo_create_mongodb.js
+++
@@ -1,8 +0,0 @@
-let MongoCliente = require("mongodb").MongoCliente;
-let url = "mongodb://localhost:27017/mydb";
-
-MongoCliente.connect(url,function(err, db) {
-    if (err) throw err;
-    console.log("Database created!");
-    db.close();
-});
