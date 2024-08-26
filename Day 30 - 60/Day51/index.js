0f1dbf884c3fa2647c8deabfb334583ce5528c78
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-08-26 20:51:38 -0300

    Day51

---
+++ Day 30 - 60/Day51/index.js
@@ -0,0 +1,81 @@
+const inputTarefa = document.querySelector(".input-tarefa");
+const btnTarefa = document.querySelector(".btn-tarefa");
+const tarefas = document.querySelector(".tarefas");
+
+function criaLi() {
+  const li = document.createElement("li");
+  return li;
+}
+
+inputTarefa.addEventListener("keypress", function (event) {
+  if (event.keyCode === 13) {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+  }
+});
+
+function limpaInput() {
+  inputTarefa.value = "";
+  inputTarefa.focus();
+}
+
+function criaBotaoApagar(li) {
+  li.innerText += " ";
+  const botaoApagar = document.createElement("button");
+  botaoApagar.innerText = "Apagar";
+  botaoApagar.setAttribute("class", "apagar");
+  li.appendChild(botaoApagar);
+}
+
+function criaTarefa(textoInput) {
+  const li = criaLi();
+  li.innerText = textoInput;
+  tarefas.appendChild(li);
+  limpaInput();
+  criaBotaoApagar(li);
+  salvarTarefa();
+}
+
+btnTarefa.addEventListener("click", function () {
+  if (!inputTarefa.value) return;
+  criaTarefa(inputTarefa.value);
+});
+
+document.addEventListener("click", function (event) {
+  const el = event.target;
+
+  if (el.classList.contains("apagar")) {
+    el.parentElement.remove();
+    salvarTarefa();
+  }
+});
+
+function salvarTarefa() {
+  const liTarefas = tarefas.querySelectorAll("li");
+  const listaDeTarefas = [];
+
+  for (let tarefa of liTarefas) {
+    let tarefaTexto = tarefa.innerText;
+    tarefaTexto = tarefaTexto.replace("Apagar", "").trim();
+    listaDeTarefas.push(tarefaTexto);
+  }
+
+  const tarefasJSON = JSON.stringify(listaDeTarefas);
+  localStorage.setItem("tarefas", tarefasJSON);
+}
+
+function adicionaTarefasSalvas() {
+  const tarefas = localStorage.getItem("tarefas");
+
+  // Verifica se o valor não é null e se é um array válido
+  if (tarefas) {
+    const listaDeTarefas = JSON.parse(tarefas);
+
+    if (Array.isArray(listaDeTarefas)) {
+      for (let tarefa of listaDeTarefas) {
+        criaTarefa(tarefa);
+      }
+    }
+  }
+}
+adicionaTarefasSalvas();
