d4b36d6cb6d41d63a408721fd8489f25280acaff
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-08-29 21:07:37 -0300

    Day 53

--- Day 30 - 60/Day52/index.js
+++ Day 30 - 60/Day52/index.js
@@ -1,32 +1,36 @@
-let listaTelefonica = {}
+let listaTelefonica = {};
 
 function incluir(nome, numero) {
-    listaTelefonica[nome] = numero;
-    return `${nome} incluído com sucesso.`
-};
+  listaTelefonica[nome] = numero;
+  if (nome == undefined && numero == undefined) {
+    console.error("Adicione os valores!!");
+  } else {
+    return `${nome} incluído com sucesso na lista de contatos.`;
+  }
+}
 
 function excluir(nome) {
-    if (nome in listaTelefonica) {
-        delete listaTelefonica[nome];
-        return `${nome} excluído com sucesso.`
-    } else {
-        return `${nome} não pode ser excluído.`
-    }
-};
+  if (nome in listaTelefonica) {
+    delete listaTelefonica[nome];
+    return `${nome} excluído com sucesso.`;
+  } else {
+    return `${nome} não pode ser excluído.`;
+  }
+}
 
 function pesquisar(nome) {
-    if (nome in listaTelefonica) {
-        return `O número de ${nome} é ${listaTelefonica[nome]} `
-    } else {
-        return `${nome} não encontrado na lista.`
-    };
-};
+  if (nome in listaTelefonica) {
+    return `O número de ${nome} é ${listaTelefonica[nome]} `;
+  } else {
+    return `${nome} não encontrado na lista de contatos.`;
+  }
+}
 
-console.log(incluir('mauricio', 3829102));
-console.log(incluir('marcelo', 3823901));
-console.log(incluir('matheus', 3829320));
-console.log(excluir('mario'));
-console.log(excluir('matheus'));
-console.log(pesquisar('mauricio'));
-console.log(pesquisar('mauro'));
-console.log(listaTelefonica)
+console.log(incluir("mauricio", 3829102));
+console.log(incluir("marcelo", 3823901));
+console.log(incluir("matheus", 3829320));
+console.log(excluir("mario"));
+console.log(excluir("matheus"));
+console.log(pesquisar("mauricio"));
+console.log(pesquisar("mauro"));
+console.log(listaTelefonica);
---
+++ Day 30 - 60/Day53/index1.js
@@ -0,0 +1,28 @@
+// --------------- DECLARAÇÕES DE FUNÇÕES ------------------ //
+
+// Declaração de função (Function hoisting)
+falaOi();
+function falaOi() {
+    console.log("Oie");
+}
+
+// First-class object (Objetos de primeira classe)
+// Function expression
+const souUmDado = function () {
+    console.log("Sou um dado.");
+};
+souUmDado();
+
+// Arrow function
+const funcaoArrow = () => {
+    console.log("Sou uma arrow function");
+};
+funcaoArrow();
+
+// Dentro de um objeto
+const obj = {
+    falar: function() {
+        console.log("Estou falando...");
+    }
+};
+obj.falar();
