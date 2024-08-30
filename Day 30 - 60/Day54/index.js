2cbbc53d541184834a483c6e60e80acaa93503c4
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-08-30 15:09:47 -0300

    Day54 - 1

---
+++ Day 30 - 60/Day54/index.js
@@ -0,0 +1,11 @@
+function conta(operador, acumulador, ...numeros) {
+    for(let numero of numeros) {
+        if (operador === '+') acumulador += numero;
+        if (operador === '-') acumulador -= numero;
+        if (operador === '/') acumulador /= numero;
+        if (operador === '*') acumulador *= numero;
+    }
+
+    console.log(acumulador);
+}
+conta('/', 1, 20, 30, 40, 50);
