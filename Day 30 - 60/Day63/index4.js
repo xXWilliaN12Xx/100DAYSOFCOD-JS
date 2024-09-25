cb4dc1372bcf08ea6315488849b60103ed7f29d4
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-09-25 08:27:45 -0300

    Day 63 - 4

---
+++ Day 30 - 60/Day63/index4.js
@@ -0,0 +1,3 @@
+let cores = ['vermelho', 'verde', 'azul'];
+cores.splice(1, 1, 'amarelo', 'roxo'); // Substitui 'verde' por 'amarelo' e 'roxo'
+console.log(cores) // Resultado: ['vermelho', 'amarelo', 'roxo', 'azul']
