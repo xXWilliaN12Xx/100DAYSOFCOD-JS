e578f86bf141393b07af03f79f4b2ea5985ad5e1
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-09-25 08:25:11 -0300

    Day 63 - 3

---
+++ Day 30 - 60/Day63/index3.js
@@ -0,0 +1,5 @@
+let animais = ['cão', 'gato', 'pássaro'];
+// Adiciona dois elementos
+animais.splice(2, 0, 'coelho', 'tartaruga');
+console.log(animais);
+// Resultado: ['cão', 'gato', 'coelho', 'tartaruga', 'pássaro']
