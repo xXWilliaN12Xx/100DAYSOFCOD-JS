cd44963b2eb592fe111af5c0919f859cbec9d244
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-09-25 08:19:25 -0300

    Day 63 - 1

---
+++ Day 30 - 60/Day63/index.js
@@ -0,0 +1,7 @@
+//                0        1        2          3         4
+const nomes = ["maria", "joão", "eduardo", "gabriel", "julia"];
+
+// nomes.splice(indice, delete, elemento1, elemento2, elemento3)
+// pop
+nomes.splice(4);
+console.log(nomes);
