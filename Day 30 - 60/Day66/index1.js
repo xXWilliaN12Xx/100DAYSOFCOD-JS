817896234daf58ff8c8323ffa17c9edd669ae595
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-10-16 21:49:38 -0300

    Day 66 -1

---
+++ Day 30 - 60/Day66/index1.js
@@ -0,0 +1,5 @@
+const numeros = [1, 2, 3, 4];
+const dobrados = numeros.map(num => num * 2);
+
+console.log(dobrados); // [2, 4, 6, 8]
+console.log(numeros); // [1, 2, 3, 4] (array original inalterado)
