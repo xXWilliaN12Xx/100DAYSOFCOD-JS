8f4de9ef190a4b14cc61f8ded8aea348164e9922
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-10-21 12:18:32 -0300

    Update index1.js

--- Day 30 - 60/Day68/index1.js
+++ Day 30 - 60/Day68/index1.js
@@ -1,15 +1,15 @@
 const produtos = [
-    {nome: 'Camisa', preco: 50, emEstoque: true},
-    {nome: 'Calça', preco: 80, emEstoque: false},
-    {nome: 'Tênis', preco: 120, emEstoque: true},
-    {nome: 'Boné', preco: 30, emEstoque: true}
+  { nome: "Camisa", preco: 50, emEstoque: true },
+  { nome: "Calça", preco: 80, emEstoque: false },
+  { nome: "Tênis", preco: 120, emEstoque: true },
+  { nome: "Boné", preco: 30, emEstoque: true },
 ];
 
-const emEstoque = produtos.filter(produto => produto.emEstoque);
+const emEstoque = produtos.filter((produto) => produto.emEstoque);
 
-const comDesconto = emEstoque.map(produto => ({
-    ...produto,
-    preco: produto.preco * 0.9
+const comDesconto = emEstoque.map((produto) => ({
+  ...produto,
+  preco: produto.preco * 0.9,
 }));
 
 const total = comDesconto.reduce((acum, produto) => acum + produto.preco, 0);
