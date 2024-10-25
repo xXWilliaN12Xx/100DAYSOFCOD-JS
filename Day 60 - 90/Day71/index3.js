7127c420954707a666803a884eca1cba3d21fec8
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-10-25 16:25:05 -0300

    Day71 -3

---
+++ Day 60 - 90/Day71/index3.js
@@ -0,0 +1,23 @@
+const produto = {};
+let precoPrivado = 0;
+
+Object.defineProperty(produto, 'preco', {
+    get() {
+        return `R$ ${precoPrivado.toFixed(2)}`;
+    },
+    set(novoPreco) {
+        if (novoPreco >= 0) {
+            precoPrivado = novoPreco;
+        } else {
+            console.error('O preço não pode ser negativo!');
+        }
+    },
+    enumerable: true,
+    configurable: false
+});
+
+produto.preco = 100.5
+console.log(produto.preco);
+
+produto.preco = -10; // Tenta definir um preço inválido
+// Erro: O preço não pode ser negativo!
