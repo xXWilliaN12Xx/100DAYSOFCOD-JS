019a72498485ebf29790decc3521b23a1f6fe6c2
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-10-25 16:18:33 -0300

    Day71 -2

---
+++ Day 60 - 90/Day71/index2.js
@@ -0,0 +1,15 @@
+const pessoa = {};
+
+// Definindo uma propriedade 'nome'
+Object.defineProperty(pessoa, 'nome', {
+    value: 'Ana',
+    writable: false, // Não pode ser alterado
+    enumerable: true, // Será visível em 'Object.keys'
+    configurable: false // Não pode ser reconfigurada ou apagada
+});
+
+console.log(pessoa.nome); // Ana
+
+// Tentando alterar o nome (não funciona por causa de writable: false)
+pessoa.nome = 'carlos';
+console.log(pessoa.nome); // Ana
