06bffbbfa9540fcaa953b029e2156a18431379a0
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-10-29 15:03:42 -0300

    Update index1.js

--- Day 60 - 90/Day72/index1.js
+++ Day 60 - 90/Day72/index1.js
@@ -1,3 +1,12 @@
-const objeto = {nome: 'Maria'};
-const descritor = Object.getOwnPropertyDescriptor(objeto, 'nome');
-console.log(descritor)
+const objeto = { nome: "Maria" };
+const descritor = Object.getOwnPropertyDescriptor(objeto, "nome");
+console.log(descritor);
+/*
+Saída:
+{
+  value: 'Maria',
+  writable: true,
+  enumerable: true,
+  configurable: true
+}
+*/
