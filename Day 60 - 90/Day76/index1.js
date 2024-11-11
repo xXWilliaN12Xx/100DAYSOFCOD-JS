971957c314523bb03ae8b149e1e3e16fd82808e0
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-11-10 21:58:53 -0300

    Day 76

---
+++ Day 60 - 90/Day76/index1.js
@@ -0,0 +1,54 @@
+function Conta(agencia, conta, saldo) {
+  this.agencia = agencia;
+  this.conta = conta;
+  this.saldo = saldo;
+}
+
+Conta.prototype.sacar = function (valor) {
+  if (valor > this.saldo) {
+    console.log(`Saldo insuficiente: ${this.saldo}`);
+    return;
+  }
+
+  this.saldo -= valor;
+  this.verSaldo();
+};
+Conta.prototype.depositar = function (valor) {
+  this.saldo += valor;
+  this.verSaldo();
+};
+Conta.prototype.verSaldo = function () {
+  console.log(
+    `Ag/c: ${this.agencia}/${this.conta} / Saldo: R$${this.saldo.toFixed(2)}`
+  );
+};
+
+function ContaCorrente(agencia, conta, saldo, limite) {
+  Conta.call(this, agencia, conta, saldo);
+  this.limite = limite;
+}
+ContaCorrente.prototype = Object.create(Conta.prototype);
+ContaCorrente.prototype.constructor = ContaCorrente;
+
+ContaCorrente.prototype.sacar = function (valor) {
+  if (valor > this.saldo + this.limite) {
+    console.log(`Saldo insuficiente: ${this.saldo}`);
+    return;
+  }
+
+  this.saldo -= valor;
+  this.verSaldo();
+};
+function ContaPoupanca(agencia, conta, saldo) {
+  Conta.call(this, agencia, conta, saldo);
+}
+ContaPoupanca.prototype = Object.create(Conta.prototype);
+ContaPoupanca.prototype.constructor = ContaPoupanca;
+
+const contacorrente = new ContaCorrente(11, 22, 0, 100);
+contacorrente.depositar(10);
+contacorrente.sacar(110);
+
+const contapoupanca = new ContaPoupanca(12, 33, 0);
+contapoupanca.depositar(10);
+contapoupanca.sacar(110);
