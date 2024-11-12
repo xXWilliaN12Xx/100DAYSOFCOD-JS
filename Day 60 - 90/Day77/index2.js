78e1dd4b84fe429e1c1688dd9ee82d22ed5c1986
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-11-12 19:06:21 -0300

    Day 77 - 2

---
+++ Day 60 - 90/Day77/index2.js
@@ -0,0 +1,15 @@
+const pessoas = [
+  { id: 3, nome: "Luiz" },
+  { id: 2, nome: "Maria" },
+  { id: 1, nome: "Helena" },
+];
+
+const novasPessoas = new Map();
+for (const pessoa of pessoas) {
+  const { id } = pessoa;
+  novasPessoas.set(id, { ...pessoa });
+}
+
+for (const pessoas of novasPessoas.values()) {
+    console.log(pessoas);
+}
